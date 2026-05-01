ee52bdf0eaf5dee56b4e779acd821a7780311916
HariomGeete <hariomgeete2@gmail.com>
2026-05-01 12:19:14 +0530

    institute backend bug fixed

--- src/modules/tests/test.service.ts
+++ src/modules/tests/test.service.ts
@@ -1,7 +1,7 @@
 import Test from "./test.model.js";
 
 import { AppError }
-from "../../shared/utils/AppError.js";
+  from "../../shared/utils/AppError.js";
 
 type CreateTestPayload = {
 
@@ -24,93 +24,93 @@ type CreateTestPayload = {
 
 // CREATE TEST
 export const createTest =
-async (
-  payload: CreateTestPayload,
+  async (
+    payload: CreateTestPayload,
 
-  instituteId: string,
+    instituteId: string,
 
-  createdBy: string
-) => {
+    createdBy: string
+  ) => {
 
-  const test =
-    await Test.create({
+    const test =
+      await Test.create({
 
-      ...payload,
+        ...payload,
 
-      instituteId,
+        instituteId,
 
-      createdBy,
-    });
+        createdBy,
+      });
 
-  return test;
-};
+    return test;
+  };
 
 // GET TESTS
 export const getTestsByInstitute =
-async (
-  instituteId: string
-) => {
+  async (
+    instituteId: string
+  ) => {
 
-  const tests =
-    await Test.find({
-      instituteId,
-    })
+    const tests =
+      await Test.find({
+        instituteId,
+      })
 
-    .populate(
-      "batchIds",
-      "name"
-    )
+        .populate(
+          "batchIds",
+          "name"
+        )
 
-    .sort({
-      createdAt: -1,
-    });
+        .sort({
+          createdAt: -1,
+        });
 
-  return tests;
-};
+    return tests;
+  };
 
 // GET SINGLE TEST
 export const getSingleTest =
-async (
-  testId: string,
+  async (
+    testId: string,
 
-  instituteId: string
-) => {
+    instituteId: string
+  ) => {
 
-  const test =
-    await Test.findOne({
-      _id: testId,
-      instituteId,
-    })
+    const test =
+      await Test.findOne({
+        _id: testId,
+        instituteId,
+      })
 
-    .populate(
-      "batchIds",
-      "name"
-    );
+        .populate(
+          "batchIds",
+          "name"
+        );
 
-  if (!test) {
+    if (!test) {
 
-    throw new AppError(
-      "Test not found",
-      404
-    );
-  }
+      throw new AppError(
+        "Test not found",
+        404
+      );
+    }
 
-  return test;
-};
+    return test;
+  };
 
 // UPDATE TEST
 export const updateTest =
 async (
   testId: string,
-
   instituteId: string,
-
-  payload: Partial<CreateTestPayload>
+  payload: any
 ) => {
 
   const test =
     await Test.findOne({
+
       _id: testId,
+
       instituteId,
     });
 
@@ -134,27 +134,27 @@ async (
 
 // DELETE TEST
 export const deleteTest =
-async (
-  testId: string,
+  async (
+    testId: string,
 
-  instituteId: string
-) => {
+    instituteId: string
+  ) => {
 
-  const test =
-    await Test.findOne({
-      _id: testId,
-      instituteId,
-    });
+    const test =
+      await Test.findOne({
+        _id: testId,
+        instituteId,
+      });
 
-  if (!test) {
+    if (!test) {
 
-    throw new AppError(
-      "Test not found",
-      404
-    );
-  }
+      throw new AppError(
+        "Test not found",
+        404
+      );
+    }
 
-  await test.deleteOne();
+    await test.deleteOne();
 
-  return true;
-};
+    return true;
+  };
